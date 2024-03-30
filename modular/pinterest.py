2ae5b913d6795f172c691902286880537d21afa1
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 21:23:44 +0700

    fixing

--- modular/pinterest.py
+++ modular/pinterest.py
@@ -1,7 +1,8 @@
 import os
-from subprocess import PIPE, Popen
-
+import aiofiles
+import aiohttp
 import requests
+from pyquery import PyQuery as pq
 from pyrogram import *
 
 from Mix import *
@@ -9,32 +10,39 @@ from Mix import *
 __modles__ = "Pinterest"
 __help__ = get_cgr("help_pint")
 
-
-async def download_m3u8_and_convert_to_mp4(m3u8_link, chat_id, caption=None):
+async def get_download_url_and_download(link, chat_id, caption=None):
     em = Emojik()
     em.initialize()
     try:
-        response = requests.get(m3u8_link)
-        with open("temp.m3u8", "wb") as f:
-            f.write(response.content)
-        await convert_m3u8_to_mp4("temp.m3u8", "temp.mp4")
-        if caption:
-            await nlx.send_video(chat_id, "temp.mp4", caption=caption)
-        else:
-            await nlx.send_video(chat_id, "temp.mp4")
-        os.remove("temp.m3u8")
-        os.remove("temp.mp4")
+        post_request = requests.post(
+            "https://www.expertsphp.com/download.php", data={"url": link}
+        )
+        request_content = post_request.content
+        str_request_content = str(request_content, "utf-8")
+        download_url = pq(str_request_content)("table.table-condensed")("tbody")("td")(
+            "a"
+        ).attr("href")
+
+        if download_url is None:
+            await nlx.send_message(chat_id, cgr("pint_3").format(em.gagal, link))
+            return
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(download_url) as resp:
+                if resp.status == 200:
+                    file_path = f"pinterest_content"
+                    async with aiofiles.open(file_path, mode="wb") as f:
+                        await f.write(await resp.read())
+
+                    if caption:
+                        await nlx.send_photo(chat_id, file_path, caption=caption)
+                    else:
+                        await nlx.send_video(chat_id, file_path, caption=caption)
+
+                    os.remove(file_path)
 
     except Exception as e:
         await nlx.send_message(chat_id, cgr("pint_4").format(em.gagal, str(e)))
-
-
-async def convert_m3u8_to_mp4(m3u8_input_path, mp4_output_path):
-    command = ["ffmpeg", "-i", m3u8_input_path, "-c", "copy", mp4_output_path]
-    process = Popen(command, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = process.communicate()
-    if process.returncode != 0:
-        print("Error:", stderr.decode())
 
 
 @ky.ubot("pint", sudo=True)
@@ -45,20 +53,9 @@ async def _(c: nlx, m):
     gue = c.me.mention
     try:
         url = m.text.split(maxsplit=1)[1]
-
-        if url.endswith((".jpg", ".jpeg", ".png")):
-            await nlx.send_photo(
-                m.chat.id, url, caption=cgr("pint_2").format(em.sukses, gue)
-            )
-        elif url.endswith(".m3u8"):
-            await download_m3u8_and_convert_to_mp4(
-                url, m.chat.id, caption=cgr("pint_2").format(em.sukses, gue)
-            )
-        else:
-            await nlx.send_document(
-                m.chat.id, url, caption=cgr("pint_2").format(em.sukses, gue)
-            )
-
+        await get_download_url_and_download(
+            url, m.chat.id, caption=cgr("pint_2").format(em.sukses, gue)
+        )
         await pros.delete()
     except Exception as e:
         await m.reply(cgr("err").format(em.gagal, str(e)))
