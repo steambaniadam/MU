4dc79b28ae08bdeeca46cd19a326788f7501b677
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 16:32:35 +0700

    fixing

--- modular/anilist.py
+++ modular/anilist.py
@@ -1,10 +1,19 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+import re
 
 from Mix import *
 
-__modles__ = "Anime Video"
+
+__modles__ = "Anime Streaming"
 __help__ = "Anime Streaming"
+
+
+URL_REGEX = re.compile(r"""(?i)\b((?:https?://|www\d{0,3}[.]|
+                          [a-z0-9.\-][.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|
+                          (\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
+                          ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip())
 
 
 def get_streaming_links(anime_id):
@@ -18,20 +27,38 @@ def get_streaming_links(anime_id):
 
 
 @ky.ubot("streaming", sudo=True)
-async def send_streaming_links(c: nlx, m):
-    if len(m.command) > 1:
-        anime_id = m.command[1]
+async def send_streaming_links(client, message):
+    if len(message.command) > 1:
+        anime_id = message.command[1]
         streaming_links = get_streaming_links(anime_id)
         if streaming_links:
-            buttons = []
-            for link_data in streaming_links:
-                name = link_data.get("name", "Unknown")
-                url = link_data.get("url", "")
-                button = InlineKeyboardButton(name, url=url)
-                buttons.append([button])
-            reply_markup = InlineKeyboardMarkup(buttons)
-            await m.reply("Pilih platform streaming:", reply_markup=reply_markup)
+            buttons_list = [(link_data["name"], link_data["url"]) for link_data in streaming_links]
+            keyboard_markup = create_keyboard(buttons_list)
+            await message.reply_text("Pilih platform streaming:", reply_markup=keyboard_markup)
         else:
-            await m.reply("Tidak ada informasi streaming untuk anime tersebut.")
+            await message.reply_text("Tidak ada informasi streaming untuk anime tersebut.")
     else:
-        await m.reply(f"Format perintah salah. Gunakan `{m.text}` [ID Anime]")
+        await message.reply_text("Format perintah salah. Gunakan /streaming [ID Anime]")
+
+
+def create_keyboard(buttons_list):
+    keyboard_data = dict()
+    for text, data in buttons_list:
+        keyboard_data[text] = data
+
+    keyboard = ikb(keyboard_data)
+    return keyboard
+
+
+def ikb(data, row_width=2):
+    buttons = []
+    for text, data in data.items():
+        if is_url(data):
+            buttons.append(InlineKeyboardButton(text, url=data))
+        else:
+            buttons.append(InlineKeyboardButton(text, callback_data=data))
+    return InlineKeyboardMarkup([buttons])
+
+
+def is_url(text):
+    return bool(URL_REGEX.match(text))
