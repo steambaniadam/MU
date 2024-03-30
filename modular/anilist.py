f0d89669b0f34afd627246a25eb53a6cc97016bf
jonesroot <jonesroot@users.noreply.github.com>
2024-03-30 10:07:35 +0000

    Auto Fix

--- modular/anilist.py
+++ modular/anilist.py
@@ -1,7 +1,4 @@
-import re
 
-import requests
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from Mix import *
 
@@ -16,13 +13,12 @@ async def _(c: nlx, m):
         try:
             xi = await c.get_inline_bot_results(bot.me.username, f"steam_in {anime_id}")
             await m.delete()
-            await c.send_inline_bot_result(m.chat.id, xi.query_id, xi.results[0].id, reply_to_message_id=ReplyCheck(m))
+            await c.send_inline_bot_result(
+                m.chat.id,
+                xi.query_id,
+                xi.results[0].id,
+                reply_to_message_id=ReplyCheck(m),
+            )
         except Exception as e:
             await m.edit(f"{e}")
             return
-        
-        
-
-        
-        
-        
