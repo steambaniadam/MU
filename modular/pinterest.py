b7fedbd88232cad82c689462fb149ca342f213e1
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 19:41:27 +0700

    fixing

--- modular/pinterest.py
+++ modular/pinterest.py
@@ -6,6 +6,7 @@ import urllib.request
 import requests
 from bs4 import BeautifulSoup
 from pyrogram import *
+from pyquery import PyQuery as pq
 
 from Mix import *
 
@@ -13,103 +14,45 @@ __modles__ = "Pinterest"
 __help__ = "Pinterest"
 
 
-async def getJson(html):
-    soup = BeautifulSoup(html, "html.parser")
-
-    scriptTag = soup.find("script", {"id": "__PWS_DATA__", "type": "application/json"})
-
-    return json.loads(re.findall(r">(.*)<", str(scriptTag))[0])
-
-
-async def getHtml(url):
-    try:
-        response = requests.get(url)
-        soup = BeautifulSoup(response.content, "html.parser")
-
-        og_image = soup.find("meta", property="og:image")
-        if og_image and og_image["content"]:
-            return og_image["content"], response.text
-
-        img_tag = soup.find("img", class_="hCL kVc L4E MIw")
-        if img_tag and img_tag.has_attr("currentSrc"):
-            return img_tag["currentSrc"], response.text
-
-        return None, None
-
-    except Exception as e:
-        print(e)
-        return None, None
+async def get_download_url(link):
+    post_request = requests.post('https://www.expertsphp.com/download.php', data={'url': link})
+    request_content = post_request.content
+    str_request_content = str(request_content, 'utf-8')
+    download_url = pq(str_request_content)('table.table-condensed')('tbody')('td')('a').attr('href')
+    return download_url
 
 
-async def decideType(jsonData):
-    response = jsonData["props"]["initialReduxState"]["pins"]
-
-    if not bool(response):
-        return None, None
-
-    _id = [*response.keys()][0]
-    data = response[_id]
-
-    if data["videos"]:
-        return 0, data["videos"]["video_list"]["V_720P"]["url"]
-
-    if data["embed"]:
-        return 1, data["embed"]["src"]
-
-    return 2, data["images"]["orig"]["url"]
+async def download_file(url, file_path, chat_id):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(file_path, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+                await bot.send_message(chat_id, "Download selesai.")
 
 
 @ky.ubot("pint", sudo=True)
 async def _(c: nlx, m):
     try:
         url = m.text.split(maxsplit=1)[1]
-        MEDIA_URL, HTML = await getHtml(url)
+        download_url = await get_download_url(url)
 
-        if MEDIA_URL is None:
+        if download_url is None:
             await m.reply_text(
-                "[ERROR] Tidak ada gambar yang ditemukan di URL Pinterest"
+                "[ERROR] Tidak ada gambar atau video yang ditemukan di URL Pinterest"
             )
             return
 
-        JSON = await getJson(HTML)
-        TYPE, MEDIA_URL = await decideType(JSON)
+        if '.mp4' in download_url:
+            file_extension = '.mp4'
+        else:
+            file_extension = '.jpg'
 
-        if TYPE is None:
-            await m.reply_text("[404] Tidak Ada yang Ditemukan!")
-            return
-
-        content_type, _ = mimetypes.guess_type(MEDIA_URL)
-        file_extension = mimetypes.guess_extension(content_type)
-
-        if file_extension is None:
-            await m.reply_text("Tidak dapat menentukan ekstensi file yang sesuai.")
-            return
-
-        file_name = f"{MEDIA_URL.split('/')[-1].split('.')[0]}{file_extension}"
+        file_name = f"pinterest_content{file_extension}"
         file_path = f"Pypin/{file_name}"
 
-        reporthook_called = False
-
-        async def reporthook(blocknum, blocksize, totalsize):
-            nonlocal reporthook_called
-            if not reporthook_called:
-                reporthook_called = True
-                await c.send_message(m.chat.id, "Sedang mengunduh...")
-
-            readsofar = blocknum * blocksize
-            if readsofar > totalsize:
-                readsofar = totalsize
-            percent = int((readsofar * 50 / totalsize))
-            r_size = totalsize / 1024**2
-            d_size = readsofar / 1024**2
-            pgbar = "[{}{}] ".format(
-                "█" * percent, " " * (50 - percent)
-            ) + "[{0:.2f}/{1:.2f} MB]".format(d_size, r_size)
-            await c.send_message(m.chat.id, f">>>> {pgbar}")
-
-        opener = urllib.request.URLopener()
-        opener.addheader("User-Agent", "Mozilla/5.0")
-        await opener.retrieve(MEDIA_URL, filename=file_path, reporthook=reporthook)
+        await download_file(download_url, file_path, m.chat.id)
 
         await m.reply_document(document=file_path)
     except Exception as e:
