9701aedbab08b128d8adaeed97a6b76dadf6e4c4
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 17:07:16 +0700

    fixing

--- modular/anilist.py
+++ modular/anilist.py
@@ -16,7 +16,7 @@ async def _(c: nlx, m):
         try:
             xi = await c.get_inline_bot_results(bot.me.username, f"steam_in {anime_id}")
             await m.delete()
-            await c.send_inline_bot_result(m.chat.id, xi.query_id, xi.results[0].id, reply_to_message_id=ReplyCheck(m)
+            await c.send_inline_bot_result(m.chat.id, xi.query_id, xi.results[0].id, reply_to_message_id=ReplyCheck(m))
         except Exception as e:
             await m.edit(f"{e}")
             return
