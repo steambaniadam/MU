0656531ae4fcfce34123306cda502bd52683119a
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 16:20:54 +0700

    fixing

--- modular/anilist.py
+++ modular/anilist.py
@@ -1,120 +1,38 @@
-"""
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 import requests
-from pyrogram import *
 
 from Mix import *
 
-__modules__ = "Anime List"
-__help__ = "Anime List"
+__modles__ = "Anime Video"
+__help__ = "Anime Streaming"
 
 
-@ky.ubot("anilist", sudo=True)
-async def anilist_command(c: nlx, m):
-    args = m.command
-    if len(args) < 2:
-        await m.reply("Please provide the ID of the anime.")
-        return
-
-    anime_id = args[1]
-
-    url = f"https://api.jikan.moe/v4/anime/{anime_id}/videos"
-
+def get_streaming_links(anime_id):
+    url = f"https://api.jikan.moe/v4/anime/{anime_id}/streaming"
     response = requests.get(url)
-
     if response.status_code == 200:
-        data = response.json()
-        if data and "promo" in data:
-            promos = data["promo"]
-            for promo in promos:
-                title = promo["title"]
-                youtube_id = promo["trailer"]["youtube_id"]
-                message_text = f"Title: {title}\nYouTube ID: {youtube_id}"
-                await m.reply(message_text, reply_to_message_id=ReplyCheck(m))
-        else:
-            await m.reply("No promo videos found for this anime.")
+        data = response.json().get("data", [])
+        return data
     else:
-        await m.reply(
-            f"Failed to fetch data. Status code: {response.status_code}",
-            reply_to_message_id=ReplyCheck(m),
-        )
-"""
-
-import requests
-from bs4 import BeautifulSoup
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-
-from Mix import *
-
-__modles__ = "Anime Movie"
-__help__ = "Anime Movie"
+        return []
 
 
-def get_video_url(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        iframe = soup.find("iframe")
-        if iframe and "src" in iframe.attrs:
-            video_src = iframe["src"]
-            return video_src
-        else:
-            return None
-    else:
-        return None
-
-
-def get_streaming_links(html_content):
-    soup = BeautifulSoup(html_content, "html.parser")
-    streaming_links = {}
-    divs = soup.find_all("div", class_="streaming-option")
-    for div in divs:
-        option_name = div.find("span").text.strip()
-        link = div["data-src"]
-        streaming_links[option_name] = link
-    return streaming_links
-
-
-@ky.ubot("anilist")
-async def anilist_command(c: nlx, m):
-    if len(m.command) < 3:
-        await m.reply_text(
-            f"Silakan masukkan judul anime dan episode setelah perintah `{m.text}` [judul anime] [episode]"
-        )
-        return
-
-    anime_title = "-".join(m.command[1:-1])
-    episode = m.command[-1]
-    url = f"https://oploverz.news/{anime_title}-episode-{episode}/"
-    response = requests.get(url)
-    if response.status_code == 200:
-        streaming_links = get_streaming_links(response.content)
+@ky.ubot("streaming", sudo=True)
+async def send_streaming_links(client, message):
+    if len(message.command) > 1:
+        anime_id = message.command[1]
+        streaming_links = get_streaming_links(anime_id)
         if streaming_links:
-            for option_name, link in streaming_links.items():
-                video_url = get_video_url(link)
-                if video_url:
-                    anime_title_display = " ".join(m.command[1:-1])
-                    reply_text = f"Berikut adalah tautan untuk menonton `{anime_title_display}` episode `{episode}` di Oploverz ({option_name}):"
-                    reply_markup = InlineKeyboardMarkup(
-                        [
-                            [
-                                InlineKeyboardButton(
-                                    text=f"Streaming di Oploverz ({option_name})",
-                                    url=video_url,
-                                )
-                            ]
-                        ]
-                    )
-                    await m.reply_text(reply_text, reply_markup=reply_markup)
-                    break  # Hanya ambil tautan dari opsi pertama yang berhasil
-            else:
-                await m.reply_text(
-                    f"Tidak dapat menemukan tautan video untuk {anime_title} episode {episode} di Oploverz."
-                )
+            buttons = []
+            for link_data in streaming_links:
+                name = link_data.get("name", "Unknown")
+                url = link_data.get("url", "")
+                button = InlineKeyboardButton(name, url=url)
+                buttons.append([button])
+            reply_markup = InlineKeyboardMarkup(buttons)
+            await message.reply_text("Pilih platform streaming:", reply_markup=reply_markup)
         else:
-            await m.reply_text(
-                f"Tidak dapat menemukan opsi pemutar untuk {anime_title} episode {episode} di Oploverz."
-            )
+            await message.reply_text("Tidak ada informasi streaming untuk anime tersebut.")
     else:
-        await m.reply_text(
-            "Maaf, terjadi kesalahan saat mengambil informasi dari Oploverz."
-        )
+        await message.reply_text("Format perintah salah. Gunakan /streaming [ID Anime]")
