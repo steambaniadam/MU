c2841e0e7337999b0fa19a0f3aa4f10b664624b9
jonesroot <jonesroot@users.noreply.github.com>
2024-03-30 12:41:50 +0000

    Auto Fix

--- modular/pinterest.py
+++ modular/pinterest.py
@@ -1,12 +1,7 @@
-import json
-import mimetypes
-import re
-import urllib.request
 
 import requests
-from bs4 import BeautifulSoup
-from pyrogram import *
 from pyquery import PyQuery as pq
+from pyrogram import *
 
 from Mix import *
 
@@ -15,10 +10,14 @@ __help__ = "Pinterest"
 
 
 async def get_download_url(link):
-    post_request = requests.post('https://www.expertsphp.com/download.php', data={'url': link})
+    post_request = requests.post(
+        "https://www.expertsphp.com/download.php", data={"url": link}
+    )
     request_content = post_request.content
-    str_request_content = str(request_content, 'utf-8')
-    download_url = pq(str_request_content)('table.table-condensed')('tbody')('td')('a').attr('href')
+    str_request_content = str(request_content, "utf-8")
+    download_url = pq(str_request_content)("table.table-condensed")("tbody")("td")(
+        "a"
+    ).attr("href")
     return download_url
 
 
@@ -26,7 +25,7 @@ async def download_file(url, file_path, chat_id):
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as resp:
             if resp.status == 200:
-                f = await aiofiles.open(file_path, mode='wb')
+                f = await aiofiles.open(file_path, mode="wb")
                 await f.write(await resp.read())
                 await f.close()
                 await bot.send_message(chat_id, "Download selesai.")
@@ -44,10 +43,10 @@ async def _(c: nlx, m):
             )
             return
 
-        if '.mp4' in download_url:
-            file_extension = '.mp4'
+        if ".mp4" in download_url:
+            file_extension = ".mp4"
         else:
-            file_extension = '.jpg'
+            file_extension = ".jpg"
 
         file_name = f"pinterest_content{file_extension}"
         file_path = f"Pypin/{file_name}"
