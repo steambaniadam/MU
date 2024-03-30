36a15f8d60b992acdd84eb182b006df43c6e2ce3
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 23:18:49 +0700

    fixing

--- modular/pinterest.py
+++ modular/pinterest.py
@@ -1,5 +1,4 @@
 import os
-
 import aiofiles
 import aiohttp
 import requests
@@ -12,7 +11,7 @@ __modles__ = "Pinterest"
 __help__ = get_cgr("help_pint")
 
 
-async def download_file_from_url(url, chat_id, caption=None):
+async def download_file_from_url(url, message):
     em = Emojik()
     em.initialize()
     try:
@@ -26,7 +25,7 @@ async def download_file_from_url(url, chat_id, caption=None):
         ).attr("href")
 
         if download_url is None:
-            await nlx.send_message(chat_id, cgr("pint_1").format(em.gagal))
+            await message.reply_text(cgr("pint_1").format(em.gagal))
             return
 
         file_extension = (
@@ -47,22 +46,22 @@ async def download_file_from_url(url, chat_id, caption=None):
                         await f.write(await resp.read())
 
                     if file_extension == ".jpg":
-                        await nlx.send_photo(chat_id, file_path, caption=caption)
+                        await message.reply_photo(file_path)
                     elif file_extension == ".mp4":
                         if file_extension == ".m3u8":
                             mp4_path = f"Pypin/pinterest_content.mp4"
                             await convert_m3u8_to_mp4(file_path, mp4_path)
-                            await nlx.send_video(chat_id, mp4_path, caption=caption)
+                            await message.reply_video(mp4_path)
                             os.remove(mp4_path)
                         else:
-                            await nlx.send_video(chat_id, file_path, caption=caption)
+                            await message.reply_video(file_path)
                     else:
-                        await nlx.send_document(chat_id, file_path, caption=caption)
+                        await message.reply_document(file_path)
 
                     os.remove(file_path)
 
     except Exception as e:
-        await nlx.send_message(chat_id, f"Error: {str(e)}")
+        await message.reply_text(f"Error: {str(e)}")
 
 
 async def convert_m3u8_to_mp4(m3u8_input_path, mp4_output_path):
@@ -81,9 +80,7 @@ async def _(c: nlx, m):
     gue = c.me.mention
     try:
         url = m.text.split(maxsplit=1)[1]
-        await download_file_from_url(
-            url, m.chat.id, caption=cgr("pint_2").format(em.sukses, gue)
-        )
+        await download_file_from_url(url, m)
         await pros.delete()
     except Exception as e:
         await m.reply(cgr("err").format(em.gagal, str(e)))
