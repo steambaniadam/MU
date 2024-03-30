3858b3494d27b6cf821890956aba0701f979a49c
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-30 14:44:58 +0700

    fixing

--- modular/anilist.py
+++ modular/anilist.py
@@ -51,13 +51,12 @@ __help__ = "Anime Movie"
 
 def get_streaming_link(html_content):
     soup = BeautifulSoup(html_content, "html.parser")
-    player_options = soup.find_all("div", class_="east_player_option")
-    streaming_links = {}
-    for option in player_options:
-        option_name = option.span.text.strip()
-        link = option["data-post"]
-        streaming_links[option_name] = link
-    return streaming_links
+    script_tag = soup.find("script", {"src": True})
+    if script_tag:
+        src = script_tag["src"]
+        return src
+    else:
+        return None
 
 
 @ky.ubot("anilist")
@@ -74,8 +73,9 @@ async def anilist_command(c: nlx, m):
     if response.status_code == 200:
         streaming_links = get_streaming_link(response.content)
         if streaming_links:
+            anime_title_display = " ".join(m.command[1:])
             reply_text = (
-                f"Berikut adalah tautan untuk menonton {anime_title} di Samehadaku:"
+                f"Berikut adalah tautan untuk menonton {anime_title_display} di Samehadaku:"
             )
             reply_markup = InlineKeyboardMarkup(
                 [
