20dad77ad6887bc222b55ff9f20d01441e97379d
naya1503 <118755946+naya1503@users.noreply.github.com>
2024-03-30 17:05:58 +0700

    update modular/anilist.py

--- modular/anilist.py
+++ modular/anilist.py
@@ -9,111 +9,20 @@ __modles__ = "Anime Streaming"
 __help__ = "Anime Streaming"
 
 
-ANIME_LIST = {
-    "Naruto": 20,
-    "Attack on Titan": 16498,
-    "One Piece": 21,
-    "Demon Slayer": 38000,
-    "My Hero Academia": 38408,
-    "Death Note": 1535,
-    "Fullmetal Alchemist": 5114,
-    "Sword Art Online": 11757,
-    "One Punch Man": 30276,
-    "Tokyo Ghoul": 22319,
-}
-
-
-URL_REGEX = re.compile(
-    r"""(?i)\b((?:https?://|www\d{0,3}[.]|
-              [a-z0-9.\-][.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|
-              (\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\
-              ()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))""".strip()
-)
-
-
-@ky.ubot("anime_list", sudo=True)
-async def show_anime_list(c: nlx, m):
-    buttons = [
-        InlineKeyboardButton(anime, callback_data=str(anime_id))
-        for anime, anime_id in ANIME_LIST.items()
-    ]
-    keyboard_markup = InlineKeyboardMarkup(
-        [buttons[i : i + 2] for i in range(0, len(buttons), 2)]
-    )
-    await m.reply_text("Daftar Anime:", reply_markup=keyboard_markup)
-
-
-def get_streaming_links(anime_id):
-    url = f"https://api.jikan.moe/v4/anime/{anime_id}/streaming"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json().get("data", [])
-        return data
-    else:
-        return []
-
-
 @ky.ubot("streaming", sudo=True)
-async def send_streaming_links(c: nlx, m):
+async def _(c: nlx, m):
     if len(m.command) > 1:
-        anime_name = m.command[1]
-        anime_id = anime_to_id(anime_name.lower())
-        if anime_id:
-            streaming_links = get_streaming_links(anime_id)
-            if streaming_links:
-                buttons_list = [
-                    (link_data["name"], link_data["url"])
-                    for link_data in streaming_links
-                ]
-                keyboard_markup = create_keyboard(buttons_list)
-                await m.reply_text(
-                    "Pilih platform streaming:", reply_markup=keyboard_markup
-                )
-            else:
-                await m.reply_text(
-                    "Tidak ada informasi streaming untuk anime tersebut.",
-                    reply_markup=InlineKeyboardMarkup([]),
-                )
-        else:
-            await m.reply_text("Anime tidak ditemukan.")
-    else:
-        await m.reply_text("Format perintah salah. Gunakan /streaming [Nama Anime]")
+        anime_id = m.command[1]
+        try:
+            xi = await c.get_inline_bot_results(bot.me.username, f"steam_in {anime_id}")
+            await m.delete()
+            await c.send_inline_bot_result(m.chat.id, xi.query_id, xi.results[0].id, reply_to_message_id=ReplyCheck(m)
+        except Exception as e:
+            await m.edit(f"{e}")
+            return
+        
+        
 
-
-def create_keyboard(buttons_list):
-    keyboard_data = dict()
-    for text, data in buttons_list:
-        keyboard_data[text] = data
-
-    return ikb(keyboard_data)
-
-
-def ikb(data, row_width=2):
-    buttons = []
-    for text, data in data.items():
-        if is_url(data):
-            buttons.append(InlineKeyboardButton(text, url=data))
-        else:
-            buttons.append(InlineKeyboardButton(text, callback_data=data))
-    keyboard_markup = InlineKeyboardMarkup([buttons])
-    return keyboard_markup
-
-
-def is_url(text):
-    return bool(URL_REGEX.search(text))
-
-
-def anime_to_id(anime_name):
-    anime_dict = {
-        "naruto": 20,
-        "attack on titan": 16498,
-        "one piece": 21,
-        "demon slayer": 38000,
-        "my hero academia": 38408,
-        "death note": 1535,
-        "fullmetal alchemist": 5114,
-        "sword art online": 11757,
-        "one punch man": 30276,
-        "tokyo ghoul": 22319,
-    }
-    return anime_dict.get(anime_name, None)
+        
+        
+        
