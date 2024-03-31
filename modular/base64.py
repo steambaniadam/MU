d258cf68f3c56428b64562fec6bd1db7e1971366
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-31 13:31:42 +0700

    fixing

--- modular/base64.py
+++ modular/base64.py
@@ -1,5 +1,4 @@
 import requests
-
 from Mix import *
 
 __modles__ = "Encoder"
@@ -9,27 +8,30 @@ __help__ = "Encoder"
 async def process_message(c: nlx, m, text, decode=False):
     em = Emojik()
     em.initialize()
-    encoding_type = "base64" if not decode else "base64&decode=true"
-    url = f"https://networkcalc.com/api/encoder/{text}?encoding={encoding_type}"
-    response = requests.get(url)
-    if response.status_code == 200:
-        data = response.json()
-        if not decode and "encoded" in data:
-            encoded_text = data["encoded"]
-            await m.reply(f"{em.sukses} Successfully encoded : `{encoded_text}`")
-        elif decode and "decoded" in data:
-            decoded_text = data["decoded"]
-            await m.reply(f"{em.sukses} Successfully decoded : `{decoded_text}`")
+    if text:
+        encoding_type = "base64" if not decode else "base64&decode=true"
+        url = f"https://networkcalc.com/api/encoder/{text}?encoding={encoding_type}"
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if not decode and "encoded" in data:
+                encoded_text = data["encoded"]
+                await m.reply(f"{em.sukses} **Successfully encoded :** `{encoded_text}`")
+            elif decode and "decoded" in data:
+                decoded_text = data["decoded"]
+                await m.reply(f"{em.sukses} **Successfully decoded :** `{decoded_text}`")
+            else:
+                await c.send_message(
+                    m.chat.id,
+                    f"{em.gagal} **Failed to {'encode' if not decode else 'decode'} the text**",
+                )
         else:
             await c.send_message(
                 m.chat.id,
-                f"{em.gagal}Failed to {'encode' if not decode else 'decode'} the text",
+                "{} **Failed to fetch data:** `{}`".format(em.gagal, response.status_code),
             )
     else:
-        await c.send_message(
-            m.chat.id,
-            "{} Failed to fetch data: {}".format(em.gagal, response.status_code),
-        )
+        await m.reply(f"{em.gagal} Silahkan berikan pesan, text, atau balas pesan")
 
 
 @ky.ubot("encode", sudo=True)
