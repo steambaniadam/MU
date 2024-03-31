3d60bed88f2cbbc2cf345ba1d5e40d43fef20d30
jonesroot <jonesroot@users.noreply.github.com>
2024-03-31 19:56:04 +0000

    Auto Fix

--- modular/restrict.py
+++ modular/restrict.py
@@ -10,8 +10,8 @@ import asyncio
 
 from pyrogram.enums import *
 from pyrogram.errors import *
-from pyrogram.types import ChatPermissions, ChatPrivileges
 from pyrogram.raw.functions.messages import DeleteHistory
+from pyrogram.types import ChatPermissions, ChatPrivileges
 
 from Mix import DEVS, Emojik, cgr, get_cgr, ky, nlx
 from Mix.core.parser import mention_html
