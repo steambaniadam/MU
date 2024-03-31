86be39703a07be387af0b01127247be2658d5cbd
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-03-31 16:53:30 +0700

    fixing

--- beban.py
+++ beban.py
@@ -20,19 +20,22 @@ from Mix import nlx
 async def dasar_laknat(client):
     LOGGER.info("Check whether this account is a burden or not...")
     async for bb in client.get_dialogs(limit=500):
-        if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            try:
-                await client.read_chat_history(bb.chat.id, max_id=0)
-            except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
-                continue
-            except FloodWait as e:
-                await asyncio.sleep(e.value)
+        try:
+            if bb.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
                 try:
                     await client.read_chat_history(bb.chat.id, max_id=0)
-                except:
+                except (ChannelPrivate, PeerIdInvalid, UserBannedInChannel):
                     continue
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+                    try:
+                        await client.read_chat_history(bb.chat.id, max_id=0)
+                    except:
+                        continue
+        except Exception as e:
+            LOGGER.error(f"An error occurred while processing chat: {e}")
+            continue
     LOGGER.info("Finished Read Message..")
-    # sys.exit(1)
 
 
 async def autor_gc():
