b8cf00a4a2c93271dc5b6024f8d2011588e732f8
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-04-01 19:21:01 +0700

    fixing

--- modular/joinleave.py
+++ modular/joinleave.py
@@ -35,15 +35,15 @@ async def _(c, m):
     em = Emojik()
     em.initialize()
     ceger = await m.reply(cgr("proses").format(em.proses))
-    is_admin = (c.get_chat_member(m.chat.id, c.get_me().id)).status in (
-        ChatMemberStatus.ADMINISTRATOR,
-        ChatMemberStatus.OWNER,
-    )
+    is_admin = (await c.get_chat_member(m.chat.id, c.get_me().id)).status in (
+                ChatMemberStatus.ADMINISTRATOR,
+                ChatMemberStatus.OWNER,
+            )
     try:
         if len(m.command) < 2:
             chat_id = m.chat.id
             namagece = m.chat.title
-            if chat_id in NO_GCAST and is_admin:
+            if chat_id in NO_GCAST and await is_admin:
                 await ceger.edit(
                     f"{em.gagal} Tidak bisa menggunakan perintah itu di sini!"
                 )
