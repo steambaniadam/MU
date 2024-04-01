84d47b6122820003267f3122205f626e73913256
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-04-01 19:15:54 +0700

    fixing

--- modular/joinleave.py
+++ modular/joinleave.py
@@ -40,9 +40,13 @@ async def _(c, m):
         if len(m.command) < 2:
             chat_id = m.chat.id
             namagece = m.chat.title
-            await ceger.edit(cgr("join_3").format(em.sukses, c.me.mention, namagece))
-            await c.leave_chat(chat_id)
-            return
+            if chat_id in NO_GCAST:
+                await ceger.edit(f"{em.gagal} Tidak bisa menggunakan perintah itu di sini!")
+                return
+            else:
+                await ceger.edit(cgr("join_3").format(em.sukses, c.me.mention, namagece))
+                await c.leave_chat(chat_id)
+                return
 
         chat_arg = m.command[1]
 
