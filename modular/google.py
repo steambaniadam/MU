98b3c2895548d22ac2e5aecde5c6cc0b82d91d1f
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-04-07 19:04:06 +0700

    hm

--- modular/google.py
+++ modular/google.py
@@ -1,9 +1,7 @@
 import urllib.parse
-
 import requests
 from bs4 import BeautifulSoup
 from pyrogram.types import Message
-
 from Mix import *
 
 __module__ = "Google"
@@ -22,17 +20,23 @@ def google_search(query, limit=3):
         search_results = soup.find_all("div", class_="tF2Cxc")
         results = []
         for result in search_results[:limit]:
-            h3_elements = result.find_all("h3")
-            for h3 in h3_elements:
-                if h3.text.strip():
-                    title = h3.text
-                    link_elem = h3.find_previous("a")
-                    if link_elem:
-                        link = link_elem["href"]
-                    else:
-                        link = "Link tidak ditemukan"
-                    results.append({"title": title, "link": link})
-                    break
+            h3_element = result.find("h3", class_="LC20lb")
+            if h3_element:
+                title = h3_element.text
+                link_elem = h3_element.find_previous("a")
+                if link_elem:
+                    link = link_elem["href"]
+                else:
+                    link = "Link tidak ditemukan"
+            else:
+                title = "Judul tidak ditemukan"
+                link = "Link tidak ditemukan"
+            span_element = result.find("span")
+            if span_element:
+                description = span_element.text
+            else:
+                description = "Deskripsi tidak ditemukan"
+            results.append({"title": title, "link": link, "description": description})
         return results
     else:
         print("Gagal melakukan pencarian Google")
@@ -49,7 +53,7 @@ async def google_command(c: nlx, m: Message):
     if results:
         response = f"{em.sukses} **Pertanyaan :** `{query}`\n\n"
         for i, result in enumerate(results, start=1):
-            response += f"{i}. [{result['title']}]({result['link']})\n\n"
+            response += f"{i}. [{result['title']}]({result['link']})\n{result['description']}\n\n"
         await m.reply(response, disable_web_page_preview=True)
         await pros.delete()
     else:
