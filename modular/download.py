2e8fc304736f5f16f795367b2c5136fa9aeb4093
jonesroot <jonesroot@users.noreply.github.com>
2024-04-12 06:11:57 +0000

    Auto Fix

--- modular/download.py
+++ modular/download.py
@@ -206,8 +206,10 @@ async def _(c, m):
             os.remove(files)
 
 
-import requests
 import os
+
+import requests
+
 
 def get_video_url(tweet_url):
     url = "https://twitter-x-media-download.p.rapidapi.com/media/privatefx"
@@ -216,7 +218,7 @@ def get_video_url(tweet_url):
     headers = {
         "content-type": "application/json",
         "X-RapidAPI-Key": "4a2cae52e9mshd8c855f97d1132bp1aad0ajsn3ae8a6aa9c5a",
-        "X-RapidAPI-Host": "twitter-x-media-download.p.rapidapi.com"
+        "X-RapidAPI-Host": "twitter-x-media-download.p.rapidapi.com",
     }
 
     response = requests.post(url, json=payload, headers=headers)
@@ -225,10 +227,11 @@ def get_video_url(tweet_url):
         # Parse JSON response
         data = response.json()
         # Extract video URL
-        video_url = data.get('tweet', {}).get('media', {}).get('all', [])[0].get('url')
+        video_url = data.get("tweet", {}).get("media", {}).get("all", [])[0].get("url")
         return video_url
     else:
         return None
+
 
 async def twit_dl(c: nlx, m: Message):
     em = Emojik()
