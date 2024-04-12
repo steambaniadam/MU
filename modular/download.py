ff0e3e034a8fe2f430210a77d5f4d884060fbb53
jonesroot <jonesroot@users.noreply.github.com>
2024-04-12 20:25:29 +0000

    Auto Fix

--- modular/download.py
+++ modular/download.py
@@ -343,10 +343,17 @@ async def insta_handler(c: nlx, m):
             media_url = result["url"]
             thumb_url = result["thumb"]
             if result["type"] == "image/jpeg":
-                await c.send_photo(photo=media_url, caption=f"{em.sukses} <b>Successfully Download Photo by : {c.me.menton}</b>")
+                await c.send_photo(
+                    photo=media_url,
+                    caption=f"{em.sukses} <b>Successfully Download Photo by : {c.me.menton}</b>",
+                )
                 await pros.delete()
             elif result["type"] == "video/mp4":
-                await c.send_video(video=media_url, thumb=thumb_url, caption=f"{em.sukses} <b>Successfully Download Video by : {c.me.menton}</b>")
+                await c.send_video(
+                    video=media_url,
+                    thumb=thumb_url,
+                    caption=f"{em.sukses} <b>Successfully Download Video by : {c.me.menton}</b>",
+                )
                 await pros.delete()
             else:
                 await pros.edit(f"{em.gagal} <b>Tipe media tidak didukung.</b>")
