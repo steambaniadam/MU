8131dd93c47a999b43ed80283b080ea3ab83e448
jonesroot <jonesroot@users.noreply.github.com>
2024-04-12 17:58:24 +0000

    Auto Fix

--- modular/download.py
+++ modular/download.py
@@ -276,13 +276,24 @@ async def twit(c: nlx, m):
     media_info = download_media_from_twitter(tweet_url)
 
     if media_info:
-        media_data = media_info.get("result", {}).get("core", {}).get("entities", {}).get("media", [])
+        media_data = (
+            media_info.get("result", {})
+            .get("core", {})
+            .get("entities", {})
+            .get("media", [])
+        )
         if media_data:
             for media in media_data:
-                media_url = media.get("media_url_https") if media.get("type") == "photo" else media.get("url")
+                media_url = (
+                    media.get("media_url_https")
+                    if media.get("type") == "photo"
+                    else media.get("url")
+                )
                 media_type = media.get("type")
                 if media_url:
-                    print(f"Informasi media berhasil diperoleh: {media_type}, {media_url}")
+                    print(
+                        f"Informasi media berhasil diperoleh: {media_type}, {media_url}"
+                    )
                     if media_type == "photo":
                         await c.send_photo(chat_id=m.chat.id, photo=media_url)
                     elif media_type == "video":
