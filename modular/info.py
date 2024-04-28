27c49f403cb49c87df85127ff753c42dfa5a60c4
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-04-29 05:27:01 +0700

    fixed

--- modular/info.py
+++ modular/info.py
@@ -420,7 +420,7 @@ async def _(c: nlx, m):
     admin = []
     pros = await m.reply(cgr("proses").format(em.proses))
     await sleep(1)
-    async for org in c.get_chat_members(filter=ChatMembersFilter.ADMINISTRATORS):
+    async for org in c.get_chat_members(m.chat.id, filter=ChatMembersFilter.ADMINISTRATORS):
         user = org.user
         ijin = org.privileges
         status = org.status
