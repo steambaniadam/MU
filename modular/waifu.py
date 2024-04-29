d909ba9fd0b57fd65830254a8c662a0b8a941d81
jonesroot <jonesroot@users.noreply.github.com>
2024-04-29 07:19:41 +0000

    Auto Fix

--- modular/waifu.py
+++ modular/waifu.py
@@ -196,10 +196,12 @@ async def _(c: nlx, m):
         return
 
     api_url = "https://api.waifu.im/search"
-    params = {"included_tags": [kuer],
-    "height": ">=2000",
-    "limit": kuen,
-    "byte_size": "<=10485760"}
+    params = {
+        "included_tags": [kuer],
+        "height": ">=2000",
+        "limit": kuen,
+        "byte_size": "<=10485760",
+    }
 
     response = requests.get(api_url, params=params)
 
