4a5dc36652790b8f06151f831628854d2967395d
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-01 02:47:57 +0700

    bejir gabut

--- modular/carbonara.py
+++ modular/carbonara.py
@@ -9,7 +9,7 @@
 import os
 import random
 
-from carbonnow import Carbon
+from SafoneAPI import SafoneAPI
 
 from Mix import *
 from Mix.core.tools_quote import *
@@ -19,72 +19,18 @@ __help__ = get_cgr("help_carbon")
 
 
 async def buat_bon(code, bgne, language, theme):
-    try:
-        carbon = Carbon(code=code, background=bgne, language=language, theme=theme)
-        carbon_image_path = await carbon.save("carbon_image")
-        return carbon_image_path
-    except Exception as e:
-        return f"Fitur ini sedang maintenance, silahkan coba beberapa saat lagi"
-
-
-@ky.ubot("carbon|carbonara", sudo=True)
-async def _(c, m):
-    em = Emojik()
-    em.initialize()
-    text = m.reply_to_message.text or m.reply_to_message.caption
-    acak = None
-    if not text:
-        return await m.reply(cgr("crbn_1").format(em.gagal))
-    ex = await m.reply(cgr("proses").format(em.proses))
-    try:
-        if len(m.command) == 1 and text:
-            acak = random.choice(loanjing)
-            tem = random.choice(loanjing)
-            lague = "python"
-        elif len(m.command) == 2:
-            warna = m.text.split(None, 1)[1] if len(m.command) > 1 else None
-            if warna:
-                acak = warna
-            else:
-                acak = random.choice(loanjing)
-            tem = random.choice(loanjing)
-        elif len(m.command) == 3:
-            warna = m.text.split(None, 2)[1] if len(m.command) > 2 else None
-            if warna:
-                acak = warna
-            else:
-                acak = random.choice(loanjing)
-            tema = m.text.split(None, 2)[2] if len(m.command) > 3 else None
-            if tema:
-                tem = tema
-            else:
-                tem = random.choice(loanjing)
-        elif len(m.command) == 4:
-            warna = m.text.split(None, 3)[1] if len(m.command) > 1 else None
-            if warna:
-                acak = warna
-            else:
-                acak = random.choice(loanjing)
-            tema = m.text.split(None, 3)[2] if len(m.command) > 2 else None
-            if tema:
-                tem = tema
-            else:
-                tem = random.choice(loanjing)
-            lague = m.text.split(None, 3)[3] if len(m.command) > 3 else "python"
-        else:
-            return await m.reply(cgr("crbn_1").format(em.gagal))
-
-        meg = await buat_bon(text, acak, lague, tem)
-        await m.reply_photo(
-            meg,
-            caption=cgr("crbn_2").format(
-                em.sukses, nlx.me.mention, reply_to_message_id=ReplyCheck(m)
-            ),
-        )
-        os.remove(meg)
-    except Exception as e:
-        await m.reply(f"{em.gagal} Maaf : {str(e)}")
-    await ex.delete()
+    meki = SafoneAPI()
+    bg = {
+        "backgroundColor": bgne,
+        "fontFamily": "Roboto",
+        "fontSize": "14px",
+        "language": language,
+        "theme": theme,
+    }
+    img = await meki.carbon(code, **bg)
+    with open("carbon.png", "wb") as file:
+        file.write(img.getvalue())
+    return "carbon.png"
 
 
 @ky.ubot("bglist", sudo=True)
@@ -102,24 +48,6 @@ async def _(c: nlx, m):
         await m.reply(jadi + iymek)
 
 
-"""
-async def buat_bon(code, bgne, language, theme):
-    meki = SafoneAPI()
-    bg = {
-        "backgroundColor": bgne,
-        "fontFamily": "Roboto",
-        "fontSize": "14px",
-        "language": language,
-        "theme": theme,
-    }
-    img = await meki.carbon(code, **bg)
-    with open("carbon.png", "wb") as file:
-        file.write(img.getvalue())
-    return "carbon.png"
-"""
-
-
-"""
 @ky.ubot("carbon|carbonara", sudo=True)
 async def _(c, m):
     em = Emojik()
@@ -198,4 +126,3 @@ async def _(c, m):
         await m.reply(cgr("crbn_1").format(em.gagal))
     await ex.delete()
     return
-"""
