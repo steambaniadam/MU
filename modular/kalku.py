4fecbc6048efe95f6689e2d78db298621cf4a638
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-01 03:17:22 +0700

    assag

--- modular/kalku.py
+++ modular/kalku.py
@@ -3,7 +3,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from Mix import *
 
 
-@ky.ubot(["calc", "kalkulator"], sudo=True)
+@ky.ubot("calc", sudo=True)
 async def _(c: nlx, message):
     await message.reply_text(
         " ",
