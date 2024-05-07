e5e991c526e3255936999fe01dfca65e8f2f9303
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-07 23:38:28 +0700

    fixing

--- modular/image.py
+++ modular/image.py
@@ -18,46 +18,43 @@ __models__ = "Image"
 __help__ = get_cgr("help_img")
 
 
-async def search_images(query, m, max_results=5):
+async def search_images(query, m, max_results=5, pros=None):
     url = "https://google-api31.p.rapidapi.com/imagesearch"
     headers = {
         "content-type": "application/json",
         "X-RapidAPI-Key": "24d6a3913bmsh3561d6af783658fp1a8240jsneef57a49ff14",
         "X-RapidAPI-Host": "google-api31.p.rapidapi.com",
     }
-    payload = {
-        "text": query,
-        "safesearch": "off",
-        "region": "wt-wt",
-        "color": "",
-        "size": "",
-        "type_image": "",
-        "layout": "",
-        "max_results": max_results,
-    }
     try:
+        payload = {
+            "text": query,
+            "safesearch": "off",
+            "region": "wt-wt",
+            "color": "",
+            "size": "",
+            "type_image": "",
+            "layout": "",
+            "max_results": max_results,
+        }
         response = requests.post(url, json=payload, headers=headers)
         response.raise_for_status()
         img_res = response.json().get("result", [])
+        
+        media_list = []
         for img_inf in img_res:
             image_url = img_inf.get("image")
             if image_url and image_url.endswith(".jpg"):
                 response = requests.get(image_url)
                 if response.status_code == 200:
                     img = BytesIO(response.content)
-                    media = InputMediaPhoto(img)
-                    await m.reply_media_group(
-                        [media], reply_to_message_id=ReplyCheck(m)
-                    )
-                    try:
-                        os.remove(media)
-                    except:
-                        pass
-                else:
-                    continue
+                    media_list.append(InputMediaPhoto(img))
+        await m.reply_media_group(media_list, reply_to_message_id=ReplyCheck(m))
     except Exception as e:
         print(f"Error fetching images: {e}")
-        return None
+    finally:
+        os.remove(media_list)
+        if pros:
+            await pros.delete()
 
 
 @ky.ubot("image|img", sudo=True)
@@ -66,7 +63,7 @@ async def _(c: nlx, m):
     em.initialize()
     try:
         query = ""
-        max_results = 1
+        max_results = 3
         if len(m.command) >= 2:
             query = m.text.split(None, 1)[1]
             if len(m.command) >= 3:
@@ -76,8 +73,8 @@ async def _(c: nlx, m):
             if len(m.command) == 2:
                 max_results = int(m.command[1])
 
-        await m.reply(cgr("proses").format(em.proses))
-        await search_images(query, m, max_results)
+        pros = await m.reply(cgr("proses").format(em.proses))
+        await search_images(query, m, max_results, pros)
         return
     except Exception as e:
         print(f"Error: {e}")
