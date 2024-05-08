c5922e54feb7c8a9ce7c070eeff17b36b187f4c3
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-08 18:16:56 +0700

    fixing

--- modular/fakeaction.py
+++ modular/fakeaction.py
@@ -14,16 +14,12 @@ __help__ = "Fake Action"
 async def _(c: nlx, m):
     em = Emojik()
     em.initialize()
-    pengguna, _ = await c.extract_user_and_reason(m)
-    rep = m.reply_to_message
     pros = await m.reply(cgr("proses").format(em.proses))
     await asyncio.sleep(3)
     try:
-        mention = (await c.get_users(pengguna)).mention
-    except IndexError:
-        mention = m.reply_to_message.sender_chat.title if m.reply_to_message else "Anon"
-    try:
         if len(m.command) > 1:
+            pengguna, alasan = await c.extract_user_and_reason(m)
+            mention = (await c.get_users(pengguna)).mention
             sukses = random.randint(50, 200)
             gagal = random.randint(1, 20)
             report_message = (
@@ -32,18 +28,13 @@ async def _(c: nlx, m):
                 f"{em.sukses} <b>Sukses : `{sukses}` grup.</b>\n"
                 f"{em.gagal} <b>Gagal : `{gagal}` grup.</b>"
             )
-            if len(m.command) > 2:
-                alasan = " ".join(m.command[2:])
+            if alasan:
                 report_message += f"\n\n<b>Alasan : `{alasan}`</b>"
-            await pros.edit(report_message)
-        elif rep:
-            mention = m.text.split(None, 1)[1]
-            alasan = m.text.split(None, 2)[2]
             await pros.edit(report_message)
         else:
             await pros.edit("Mohon berikan username atau user ID sebagai argumen.")
     except Exception as e:
-        await pros.edit(f"{em.gagal} Berikan Alasan!")
+        await pros.edit(f"{em.gagal} Gagal membuat laporan Global Banned: {str(e)}")
 
 
 @ky.ubot("gimute|gmut", sudo=True)
@@ -82,7 +73,7 @@ async def _(c: nlx, m):
         await pros.edit(f"{em.gagal} Berikan Alasan!")
 
 
-@ky.ubot("gikick|gikik", sudo=True)
+@ky.ubot("gikick|gkik", sudo=True)
 async def _(c: nlx, m):
     em = Emojik()
     em.initialize()
