5b6734ee55de193c3e28f06ba28a01922ff48bbd
Lucifer <38715155+jonesroot@users.noreply.github.com>
2024-05-08 17:39:28 +0700

    fixing fake action

--- modular/fakeaction.py
+++ modular/fakeaction.py
@@ -10,7 +10,7 @@ __modles__ = "Fake Action"
 __help__ = "Fake Action"
 
 
-@ky.ubot("giben", sudo=True)
+@ky.ubot("giben|gben", sudo=True)
 async def _(c: nlx, m):
     em = Emojik()
     em.initialize()
@@ -28,6 +28,78 @@ async def _(c: nlx, m):
             gagal = random.randint(1, 20)
             report_message = (
                 f"{em.warn} <b>Laporan Global Banned :</b>\n\n"
+                f"{em.profil} <b>Pengguna : {mention}</b>\n"
+                f"{em.sukses} <b>Sukses : `{sukses}` grup.</b>\n"
+                f"{em.gagal} <b>Gagal : `{gagal}` grup.</b>"
+            )
+            if len(m.command) > 2:
+                alasan = " ".join(m.command[2:])
+                report_message += f"\n\n<b>Alasan : `{alasan}`</b>"
+            await pros.edit(report_message)
+        elif rep:
+            mention = m.text.split(None, 1)[1]
+            alasan = m.text.split(None, 2)[2]
+            await pros.edit(report_message)
+        else:
+            await pros.edit("Mohon berikan username atau user ID sebagai argumen.")
+    except Exception as e:
+        await pros.edit(f"{em.gagal} Berikan Alasan!")
+
+
+@ky.ubot("gimute|gmut", sudo=True)
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    pengguna, _ = await c.extract_user_and_reason(m)
+    rep = m.reply_to_message
+    pros = await m.reply(cgr("proses").format(em.proses))
+    await asyncio.sleep(3)
+    try:
+        mention = (await c.get_users(pengguna)).mention
+    except IndexError:
+        mention = m.reply_to_message.sender_chat.title if m.reply_to_message else "Anon"
+    try:
+        if len(m.command) > 1:
+            sukses = random.randint(50, 200)
+            gagal = random.randint(1, 20)
+            report_message = (
+                f"{em.warn} <b>Laporan Global Mute :</b>\n\n"
+                f"{em.profil} <b>Pengguna : {mention}</b>\n"
+                f"{em.sukses} <b>Sukses : `{sukses}` grup.</b>\n"
+                f"{em.gagal} <b>Gagal : `{gagal}` grup.</b>"
+            )
+            if len(m.command) > 2:
+                alasan = " ".join(m.command[2:])
+                report_message += f"\n\n<b>Alasan : `{alasan}`</b>"
+            await pros.edit(report_message)
+        elif rep:
+            mention = m.text.split(None, 1)[1]
+            alasan = m.text.split(None, 2)[2]
+            await pros.edit(report_message)
+        else:
+            await pros.edit("Mohon berikan username atau user ID sebagai argumen.")
+    except Exception as e:
+        await pros.edit(f"{em.gagal} Berikan Alasan!")
+
+
+@ky.ubot("gikick|gikik", sudo=True)
+async def _(c: nlx, m):
+    em = Emojik()
+    em.initialize()
+    pengguna, _ = await c.extract_user_and_reason(m)
+    rep = m.reply_to_message
+    pros = await m.reply(cgr("proses").format(em.proses))
+    await asyncio.sleep(3)
+    try:
+        mention = (await c.get_users(pengguna)).mention
+    except IndexError:
+        mention = m.reply_to_message.sender_chat.title if m.reply_to_message else "Anon"
+    try:
+        if len(m.command) > 1:
+            sukses = random.randint(50, 200)
+            gagal = random.randint(1, 20)
+            report_message = (
+                f"{em.warn} <b>Laporan Global Kick :</b>\n\n"
                 f"{em.profil} <b>Pengguna : {mention}</b>\n"
                 f"{em.sukses} <b>Sukses : `{sukses}` grup.</b>\n"
                 f"{em.gagal} <b>Gagal : `{gagal}` grup.</b>"
